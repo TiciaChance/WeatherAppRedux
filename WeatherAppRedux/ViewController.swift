30c799ec253ca56115ec0608634f3f106e4328f4
TiciaChance <laticia.chance@flatironschool.com>
2016-11-30 14:57:43 -0500

    retrieve current and hourly data

--- WeatherAppRedux/ViewController.swift
+++ WeatherAppRedux/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var weather = WeatherAPI()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        weather.APICall(urlRequest: URL(string: "https://api.darksky.net/forecast/4f9f733e45e297b9118dc052b51e101f/40.3601,-74.0589")!)
     }
 
     override func didReceiveMemoryWarning() {
--- WeatherAppRedux/WeatherAPI.swift
+++ WeatherAppRedux/WeatherAPI.swift
@@ -7,7 +7,38 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class WeatherAPI: NSObject {
+    
+    var city = String()
+    var country = String()
+    var longitude = Double()
+    var latitude = Double()
+    
+    var DailySummary = String()
+    var currently = [String : JSON]()
+    var hourly = [String : JSON]()
+    
+    
+    func APICall(urlRequest: URL) {
+        
+        Alamofire.request("https://api.darksky.net/forecast/4f9f733e45e297b9118dc052b51e101f/40.3601,-74.0589").responseJSON { (response) in
+            
+            guard let weather = JSON(data: response.data!).dictionary else {return}
+            
+            self.currently = (weather["currently"]?.dictionaryValue)!
+            self.hourly = (weather["hourly"]?.dictionaryValue)!
+
+            
+            print("\n\n\n\nTHIS IS CURRENTLY \(self.currently)")
+            print("\n\n\n\nTHIS IS HOURLY \(self.hourly)")
+
+
+        }
+        
+        
+    }
 
 }
