0c54697f8b59d5f3d80523b850e002eeead2577a
TiciaChance <laticia.chance@flatironschool.com>
2016-11-30 13:52:25 -0500

    first commit

---
+++ WeatherAppRedux/WeatherAPI.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherAPI.swift
+//  WeatherAppRedux
+//
+//  Created by Laticia Chance on 11/30/16.
+//  Copyright © 2016 Laticia Chance. All rights reserved.
+//
+
+import UIKit
+
+class WeatherAPI: NSObject {
+
+}
