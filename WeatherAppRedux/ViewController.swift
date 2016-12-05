2f044c7ed2679a408150caf70d332a7d2477fb71
TiciaChance <laticia.chance@flatironschool.com>
2016-12-05 09:05:07 -0500

    problems with data fixed

--- WeatherAppRedux/ViewController.swift
+++ WeatherAppRedux/ViewController.swift
@@ -10,23 +10,41 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-
-    let weather = WeatherAPI()
-
+    
+    var weather = WeatherAPI()
+    
     let locationManager = CLLocationManager()
     var locationLong = Double()
     var locationLat = Double()
+    
+    @IBOutlet weak var cityLbl: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         instantiateLocationManager()
-        weather.APICall(urlRequest: URL(string: "https://api.darksky.net/forecast/4f9f733e45e297b9118dc052b51e101f/51.5074,0.1278")!)
+        weather.APICall {
+            self.updateUI()
+            print("CITY \(self.weather.city)")
+        }
+    }
+
+    
+    func updateUI() {
         
+        cityLbl.text = weather.city
+        tempLabel.text = String(weather.currentTemperature)
+
     }
     
-   
+}
+
+extension ViewController {
+    
     func instantiateLocationManager() {
         
         locationManager.requestAlwaysAuthorization()
@@ -45,13 +63,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let userLocation: CLLocation = locations[0]
         locationLong = userLocation.coordinate.longitude
         locationLat = userLocation.coordinate.latitude
-        
-        weather.getWeatherByCoordinates(latitude: locationLat, longitude: locationLong)
-        
+
         print(locationLat)
         print(locationLong)
         
+        
+        
     }
-    
 }
 
--- WeatherAppRedux/WeatherAPI.swift
+++ WeatherAppRedux/WeatherAPI.swift
@@ -10,9 +10,14 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
+
 class WeatherAPI: NSObject {
     
     let baseURL = "https://api.darksky.net/forecast/4f9f733e45e297b9118dc052b51e101f/"
+    
+    // store info into an array or dictionary
+    // access that collection in vc
+    //
     
     var city = String()
     var country = String()
@@ -37,25 +42,45 @@ class WeatherAPI: NSObject {
     var sunset = Int() //change to NSDATE
     var sunrise = Int()
     
-    
-    func APICall(urlRequest: URL) {
+    func APICall(completed: @escaping () -> ()) {
         
-        Alamofire.request(urlRequest).responseJSON { (response) in
+        Alamofire.request("https://api.darksky.net/forecast/4f9f733e45e297b9118dc052b51e101f/40,-74").responseJSON(completionHandler: {
+            response in
             
-            //guard let weather = JSON(data: response.data!).dictionary else {return}
+            guard let weather = JSON(data: response.data!).dictionary else {return}
             
-        }
-        
-    }
-    
-//    func getWeatherByCity(city: String) {
-//        let weatherRequestURL = URL(string: "\(baseURL)\(latitude)\(longitude)")
-//        APICall(urlRequest: weatherRequestURL!)
-//    }
- 
-    func getWeatherByCoordinates(latitude: Double, longitude: Double) {
-        let weatherRequestURL = URL(string: "\(baseURL)\(latitude)\(longitude)")!
-        APICall(urlRequest: weatherRequestURL)
-    }
+            let timezone = weather["timezone"]?.stringValue
+            let countryCity = timezone?.components(separatedBy: "/")
+            self.country = (countryCity?[0])!
+            self.city = (countryCity?[1].replacingOccurrences(of: "_", with: " "))!
 
+            
+            self.daily = (weather["daily"]?.dictionaryValue)!
+            self.currently = (weather["currently"]?.dictionaryValue)!
+            self.hourly = (weather["hourly"]?.dictionaryValue)!
+            
+            self.dailySummary = (self.daily["data"]?["summary"].stringValue)!
+            
+            self.currentChanceOfRain = (self.currently["precipProbability"]?.doubleValue)!
+            self.currentSummary = (self.currently["summary"]?.stringValue)!
+            self.currentTemperature = (self.currently["temperature"]?.doubleValue)!
+            self.curentHumidity = (self.currently["humidity"]?.doubleValue)!
+            self.currentWindSpeed = (self.currently["windSpeed"]?.doubleValue)!
+            
+            self.hourlyTemperature = (self.hourly["data"]![0]["temperature"].doubleValue)
+            
+            self.sunset = (self.daily["data"]?[0]["sunsetTime"].intValue)!
+            self.sunrise = (self.daily["data"]?[0]["sunriseTime"].intValue)!
+            
+            
+            print("\n\n\n\nTHIS IS CURRENTLY \(self.city)")
+            //print("\n\n\n\nTHIS IS HOURLY \(self.hourly)")
+            
+            completed()
+        })
+    }
 }
+
+
+
+
